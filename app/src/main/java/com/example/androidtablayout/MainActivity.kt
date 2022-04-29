fcf9152e26c8d1e9deb71796ee6794e87d87cc87
Tauzhnyanskiy Artiom <art121bog@gmail.com>
2022-04-29 12:59:55 +0300

    Initial commit

--- app/src/main/java/com/example/androidtablayout/MainActivity.kt
+++ app/src/main/java/com/example/androidtablayout/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.androidtablayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.androidtablayout.databinding.ActivityMainBinding
 import com.example.androidtablayout.fragments.FirstFragment
 import com.example.androidtablayout.fragments.SecondFragment
@@ -42,5 +45,12 @@ class MainActivity : AppCompatActivity() {
         TabLayoutMediator(binding.tabLayout, binding.vp2) { tab, position ->
             tab.text = "TAB ${(position + 1)}"
         }.attach()
+    }
+
+    class MyAdapter(fragment: FragmentActivity, private val list: List<Fragment>) : FragmentStateAdapter(fragment) {
+        override fun getItemCount() = list.size
+
+        override fun createFragment(position: Int) = list[position]
+
     }
 }
